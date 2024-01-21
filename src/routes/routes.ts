02e060b29c093ee5f7be17612488506f6c4c2b06
yizhong187 <yizhong187@gmail.com>
2024-01-21 23:28:03 +0800

    added account settings path, user profile path and fallback route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
-// src/routers/routes.ts
 import HomePage from "../pages/HomePage";
 import SubforumPage from "../pages/SubforumPage";
 import ThreadPage from "../pages/ThreadPage";
 import LoginPage from "../pages/LoginPage";
 import SignUpPage from "../pages/SignUpPage";
+import AccountSettingsPage from "../pages/AccountSettingsPage";
+import ProfilePage from "../pages/ProfilePage";
+import NotFoundPage from "../pages/NotFoundPage";
 
 export interface Route {
   path: string;
@@ -17,4 +19,9 @@ export const routes: Route[] = [
   { path: "/subforums/:subforumID/threads/:threadID", component: ThreadPage },
   { path: "/login", component: LoginPage },
   { path: "/signup", component: SignUpPage },
+  { path: "/account-settings", component: AccountSettingsPage },
+  { path: "/profile/:userName", component: ProfilePage },
+
+  // Fallback route for any undefined paths, leading to a '404 Not Found' page
+  { path: "*", component: NotFoundPage },
 ];
