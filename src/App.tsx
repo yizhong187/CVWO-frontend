f5ac01e11537c8b71d127b0f275f8ae5a94a6be6
yizhong187 <yizhong187@gmail.com>
2024-01-21 23:20:16 +0800

    Added LoginSnackbar and PostSnackbar Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CustomAppBar from "./components/CustomAppBar";
-import { UserContext } from "./contexts/UserContext";
-import { UserModel } from "./interfaces/UserModel";
+import CustomAppBar from "./components/Common/CustomAppBar";
 import { routes } from "./routes/routes";
-import { SnackbarProvider } from "./contexts/LoginSnackBarContext";
+import { PostSnackbarProvider } from "./contexts/PostSnackbarContext";
+import { UserProvider } from "./contexts/UserContext";
+import { LoginSnackbarProvider } from "./contexts/LoginSnackbarContext";
 
 const App: React.FC = () => {
-  const [user, setUser] = useState<UserModel | null>(null);
-
-  useEffect(() => {
-    const storedUser = sessionStorage.getItem("user");
-    if (storedUser) {
-      try {
-        const userState: UserModel = JSON.parse(storedUser);
-        setUser(userState);
-      } catch (error) {
-        console.error("Error parsing user data from session storage", error);
-        sessionStorage.removeItem("user");
-      }
-    }
-  }, []);
-
   return (
-    <SnackbarProvider>
-      <UserContext.Provider value={{ user, setUser }}>
-        <Router>
-          <CustomAppBar />
-          <Routes>
-            {routes.map((route, index) => {
-              const Component = route.component;
-              return (
-                <Route key={index} path={route.path} element={<Component />} />
-              );
-            })}
-          </Routes>
-        </Router>
-      </UserContext.Provider>
-    </SnackbarProvider>
+    <LoginSnackbarProvider>
+      <PostSnackbarProvider>
+        <UserProvider>
+          <Router>
+            <CustomAppBar />
+            <Routes>
+              {routes.map((route, index) => {
+                const Component = route.component;
+                return (
+                  <Route
+                    key={index}
+                    path={route.path}
+                    element={<Component />}
+                  />
+                );
+              })}
+            </Routes>
+          </Router>
+        </UserProvider>
+      </PostSnackbarProvider>
+    </LoginSnackbarProvider>
   );
 };
 
