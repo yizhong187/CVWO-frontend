6a7ede04fa088d0212babb09de733c8b9fba8a3d
yizhong187 <yizhong187@gmail.com>
2024-01-21 23:23:41 +0800

    added comment

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL = "http://localhost:8080/v2";
 
+// Configuring Axios to automatically send cookies with each request (used for JWT authentication)
 axios.defaults.withCredentials = true;
 
 const apiClient = axios.create({
